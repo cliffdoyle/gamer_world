c79873971e8591950571791bc27ca5358756e914
clomollo <oyoocliff471@gmail.com>
2025-04-11 09:52:08 +0300

    feat: setup environment variables and linked the postgres database with the user service

---
+++ user-service/database/db.go
@@ -0,0 +1,39 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+	
+	_ "github.com/lib/pq"
+)
+
+
+var DB *sql.DB
+
+func Connect(){
+	dbHost:=os.Getenv("DB_HOST")
+	dbPort:=os.Getenv("DB_PORT")
+	dbUser:=os.Getenv("DB_USER")
+	dbPassword:=os.Getenv("DB_PASSWORD")
+	dbName:=os.Getenv("DB_NAME")
+
+	dsn:=fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		dbHost,
+		dbPort,
+		dbUser,
+		dbPassword,
+		dbName,
+	)
+	var err error
+	DB,err=sql.Open("postgres",dsn)
+	if err !=nil{
+		log.Fatal("Failed to connect to database:",err)
+	}
+	err=DB.Ping()
+	if err !=nil{
+		log.Fatal("Failed to ping database:",err)
+	}
+	log.Println("Connected to database successfully")
+}
