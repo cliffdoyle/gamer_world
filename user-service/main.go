f56523ad9474c38e198b179d44ef2ba944d64de4
clomollo <oyoocliff471@gmail.com>
2025-04-11 23:13:33 +0300

    updated main to use the authentication middleware to protect the user endpoints

--- user-service/main.go
+++ user-service/main.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/cliffdoyle/gamer_world/user-service/database"
 	"github.com/cliffdoyle/gamer_world/user-service/handlers"
+	"github.com/cliffdoyle/gamer_world/user-service/middleware"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
@@ -21,8 +22,21 @@ func main() {
 		c.JSON(200, gin.H{"status": "User service is Up!"})
 	})
 
+	//Public routes
 	r.POST("/register", handlers.Register)
 	r.POST("/login", handlers.Login)
+
+	//Protected routes
+	userGroup := r.Group("/user")
+	userGroup.Use(middleware.AuthMiddleware())
+	{
+		userGroup.GET("/profile", handlers.GetUserProfile)
+		userGroup.PUT("/profile", handlers.UpdateUserProfile)
+		userGroup.DELETE("/profile", handlers.DeleteUserAccount)
+	}
+
+	log.Println("User service is running on port:", os.Getenv("PORT"))
+	// Start the server
 
 	r.Run(":" + os.Getenv("PORT"))
 }
