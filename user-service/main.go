c422b83e59e645393199a910c470477b003d8af0
clomollo <oyoocliff471@gmail.com>
2025-04-11 10:03:40 +0300

    fix: fixed the typo in .env file to gaming to enable ping with the database

--- user-service/main.go
+++ user-service/main.go
@@ -1,11 +1,20 @@
-package main 
+package main
 
 import (
+	"log"
+
+	"github.com/cliffdoyle/gamer_world/user-service/database"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 
 func main(){
+	err:=godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	database.Connect()
 	r:=gin.Default()
 	r.GET("/health",func(c *gin.Context){
 		c.JSON(200,gin.H{"status":"User service is Up!"})
