da8b0fd59c651c15bb846fd9afe2b7d19b219ade
clomollo <oyoocliff471@gmail.com>
2025-05-14 00:40:04 +0300

    update: added more fields useractivity interface for dashboard correlation

--- tournament-frontend/src/types/tournament.ts
+++ tournament-frontend/src/types/tournament.ts
@@ -8,6 +8,7 @@ export interface TournamentResponse {
   description: string;
   game: string;
   format: TournamentFormat;
+   currentParticipants: number;
   status: TournamentStatus;
   maxParticipants: number;
   registrationDeadline: string | null;
@@ -78,4 +79,7 @@ export interface UserActivity { // If you're using `UserActivity as BackendUserA
   related_entity_type?: string | null;
   context_url?: string | null;
   date: string; // maps to createdAt
+    rank?: string;         // Optional if not always present
+  level?: number;        // Optional
+  avatarUrl?: string;   
 }
