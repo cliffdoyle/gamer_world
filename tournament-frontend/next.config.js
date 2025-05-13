5463456d48d010cab3565dcfaa8b0d4e61d780a2
clomollo <oyoocliff471@gmail.com>
2025-05-14 00:36:43 +0300

    fix:updated nextjs configuration pattern

--- tournament-frontend/next.config.js
+++ tournament-frontend/next.config.js
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+    reactStrictMode: true, // Or whatever other configs you have
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '', // Usually empty for standard https
+        pathname: '/a/**', // Allows any path under /a/ which is common for Google user profile pics
+      },
+      // You can add other hostnames here if needed
+      // {
+      //   protocol: 'https',
+      //   hostname: 'another-image-provider.com',
+      // },
+    ],
+  },
+  
 };
 
 module.exports = nextConfig; 
