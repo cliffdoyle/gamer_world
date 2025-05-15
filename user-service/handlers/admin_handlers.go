bdb40f34099eb4f2c464a9668195a6b741b05a85
clomollo <oyoocliff471@gmail.com>
2025-05-15 17:36:11 +0300

    feat: add listuserforlinking function that retrieves a list of users suitable for linking to tournament participants

--- user-service/handlers/admin_handlers.go
+++ user-service/handlers/admin_handlers.go
@@ -1,6 +1,13 @@
 package handlers
 
-import "github.com/google/uuid"
+import (
+	"net/http"
+
+	"github.com/cliffdoyle/gamer_world/user-service/database"
+	"github.com/cliffdoyle/gamer_world/user-service/models"
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+)
 
 
 type UserForLinking struct{
@@ -8,4 +15,13 @@ type UserForLinking struct{
 	Username string `json:"username"`
 	 DisplayName string    `json:"display_name,omitempty"`
 
+}
+
+func ListUsersForLinking(c *gin.Context) {
+	var users []models.User
+	err:=database.DB.Select("id, username").Order("username asc").Find(&users).Error
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch users: " + err.Error()})
+		return
+	}
 }
